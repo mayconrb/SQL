e720371d6f27bca9cc8d1b4fc9c7e3b59d95cbd1
mayconrb <mayconrb00@gmail.com>
2024-04-01 21:43:57 -0300

    Código em SQL que filtra recupera todos as linhas/coluna de uma tabela por ordem crescente da coluna sobrenome

---
+++ Exerc8.sql
@@ -0,0 +1,8 @@
+--Escreva uma consulta em SQL para recuperar todas as linhas e colunas da tabela
+--de funcionários usando o alias de tabela no banco de dados Adventureworks.
+--Classifique a saída em ordem crescente no sobrenome.
+--Tabela de exemplo: Person.Person
+
+SELECT *
+FROM person.person AS p
+ORDER BY lastname ASC
