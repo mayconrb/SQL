d85c25d1e35140552b6aa61f59b48c2d562ca3ab
mayconrb <mayconrb00@gmail.com>
2024-04-01 22:11:07 -0300

    Cod-SQL que retornar colunas especificas de uma tabela, utilizando parametros IS NOT para NULL e filtrando por MODELO 'T', com saída organizada por ordem crescente.

---
+++ Exerc10.sql
@@ -0,0 +1,17 @@
+-- Na tabela a seguir, escreva uma consulta em SQL para retornar apenas as 
+-- linhas do produto que possuem uma data de início de venda que não seja NULL 
+-- e uma linha de produto 'T'. Retorne productid, productnumber e nome.
+-- Organizou a saída em ordem crescente no nome.
+-- Tabela de amostra: production.Product
+
+SELECT
+ productid
+ , productnumber
+ , name
+ , productline
+FROM production.product
+WHERE sellstartdate IS NOT NULL AND production.product.productline = 'T'
+ORDER BY name
+
+
+
