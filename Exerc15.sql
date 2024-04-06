1efbffbbcfb36f256bcb0e9d1a8e92083053090d
mayconrb <mayconrb00@gmail.com>
2024-04-06 00:14:29 -0300

    SQL para verificar QTD total por localização

---
+++ Exerc15.sql
@@ -0,0 +1,14 @@
+-- Na tabela a seguir, escreva uma consulta em SQL para encontrar a quantidade total para cada ID de local e calcular o total geral para todos os locais.
+-- Retorne o ID do local e a quantidade total.
+-- Agrupe os resultados em locationid.
+
+SELECT
+    locationid
+    , name AS local
+    , costrate AS custo
+    , availability AS disponibilidade
+    , ROUND ((costrate * availability), 2) AS qtd_total
+
+FROM production.location
+
+ORDER BY locationid
